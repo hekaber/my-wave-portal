4b63623e3da2b879cf534c3fccc23ddf3db37a4c
Henry Karl Berger <hkberger@gmail.com>
2022-12-21 14:39:00 +0100

    [FEATURE] Dockerize hardhat node (#2)
    * [TASK] Change filetree
    
    * [TASK] Dockerize hardat node
    
    * [TASK] add entrypoint and clean up
    
    * [FEATURE] Automatisation

--- docker/hardhat-app/hardhat.config.js
+++ docker/hardhat-app/hardhat.config.js
@@ -1,5 +1,3 @@
-require("@nomicfoundation/hardhat-toolbox");
-
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
