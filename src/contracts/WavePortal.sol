ed02e6b194972ee039a6db9203925ba68dcce673
Karl <hkberger@gmail.com>
2022-12-25 18:26:32 +0100

    [TASK] Implement contract balance

--- src/contracts/WavePortal.sol
+++ src/contracts/WavePortal.sol
@@ -19,11 +19,9 @@ contract WavePortal {
     //Store the waves
     Wave[] waves;
 
-    constructor() {
-    }
+    constructor() payable {}
 
     function wave(string memory _message) public {
-
         totalWaves += 1;
         console.log("%s waved w/ message %s", msg.sender, _message);
 
@@ -31,6 +29,15 @@ contract WavePortal {
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
         emit NewWave(msg.sender, block.timestamp, _message);
+
+        uint256 prizeAmount = 0.0001 ether;
+        // check if I have enough in my wallet to afford the prize
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
     }
 
     /*
