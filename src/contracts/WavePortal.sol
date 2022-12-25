9f32116ed8b621515c516904dfaed271df8b6287
Karl <hkberger@gmail.com>
2022-12-25 19:21:47 +0100

    [TASK] Decrease participation waiting time

--- src/contracts/WavePortal.sol
+++ src/contracts/WavePortal.sol
@@ -21,7 +21,7 @@ contract WavePortal {
 
     //Store the waves
     Wave[] waves;
-    
+
     mapping(address => uint256) public lastWavedAt;
 
     constructor() payable {
@@ -34,9 +34,13 @@ contract WavePortal {
          /*
          * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
          */
-        require(
+        /* require(
             lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
             "Wait 15m"
+        ); */
+        require(
+            lastWavedAt[msg.sender] + 30 seconds < block.timestamp, 
+            "Must wait 30 seconds before waving again."
         );
 
         /*
